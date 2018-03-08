aa5e3f2a48e5e9164a1a44a0a74a284e3a0dea45
Imre Bartis <imre.bartis@siili.com>
2018-03-08 06:13:24 +0200

    components

---
+++ zoo/src/components/Zoo.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="row">
+    <h2>Here's your Zoo</h2>
+    <ol>
+      <li v-bind:key="animal.id" v-for="animal in animals"><span class="animal" :animal="animalsCodes[animal]"></span></li>
+    </ol>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['animals', 'animalsCodes', 'name']
+}
+</script>
