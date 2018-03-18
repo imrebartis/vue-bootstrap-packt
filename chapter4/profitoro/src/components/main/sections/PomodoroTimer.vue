19b2de262b0cf7baf767f6b7e02500ea4ed048f2
Imre Bartis <imre.bartis@siili.com>
2018-03-18 06:19:40 +0200

    count down works

--- chapter4/profitoro/src/components/main/sections/PomodoroTimer.vue
+++ chapter4/profitoro/src/components/main/sections/PomodoroTimer.vue
@@ -1,9 +1,14 @@
 <template>
-  <count-down-timer></count-down-timer>
+  <count-down-timer :time="time"></count-down-timer>
 </template>
 <script>
   import CountDownTimer from './timer/CountDownTimer'
   export default {
+    computed: {
+      time () {
+        return 25 * 60
+      }
+    },
     components: {
       CountDownTimer
     }
